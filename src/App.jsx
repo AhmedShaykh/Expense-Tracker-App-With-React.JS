6ca57ff04e135a37dd5c03caeb0f0f1d1f5d67f0
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-02-20 21:13:05 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Heading from './Components/Heading';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Expense Tracker App
-      </h1>
+    <div className='container'>
+      <Heading />
     </div>
   );
 }
