c871f7f1a949cb1f4dfbe226e0ffe39df157defa
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-24 22:35:53 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
-      <h1>
-        Hello World
-      </h1>
+      <Header />
     </div>
   );
 }
