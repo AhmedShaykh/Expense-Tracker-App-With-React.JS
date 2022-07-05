c92428d43f3a67699c8e4061a8b8d1aea695f16f
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-07-05 23:30:29 +0500

    It's Time To Push !

--- src/Components/AddTransaction.jsx
+++ src/Components/AddTransaction.jsx
@@ -4,11 +4,11 @@ const AddTransaction = () => {
             <form className="transaction-form">
                 <label>
                     <h3>Enter Description <br /></h3>
-                    <input type="text" placeholder="Add Transaction" className="input" required/>
+                    <input type="text" placeholder="  Add Transaction" className="input" required/>
                 </label>
                 <label>
                     <h3>Enter Amount <br /></h3>
-                    <input type="number" placeholder="Add Amount" className="input" required/>
+                    <input type="number" placeholder="  Add Amount" className="input" required/>
                 </label>
                 <button className="button">Add Transaction</button>
             </form>
