9cb8c388cae2ba3ac45c8131ca7a57f7bcd7e012
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-07-18 15:48:48 +0500

    It's Time To Push !

--- src/Components/History.jsx
+++ src/Components/History.jsx
@@ -1,24 +1,26 @@
 function History() {
+
+    let transaction = [
+        { amount: 1000, desc: "Cash" },
+        { amount: -250, desc: "Gas Bill" },
+        { amount: -490, desc: "Medicines" }
+    ]
     return (
         <div className="history">
-            <h3>
+            <h2>
                 History
-            </h3>
+            </h2>
             <hr />
-            <ul className="transaction-list">
-                <li>
-                    <span>Cash</span>
-                    <span>+ 500</span>
-                </li>
-                <li>
-                    <span>Cash</span>
-                    <span>+ 500</span>
-                </li>
-                <li>
-                    <span>Cash</span>
-                    <span>+ 500</span>
-                </li>
-            </ul>
+            <h3>
+                <ul className="transaction-list">
+                    {transaction.map((obj, ind) => {
+                        return (<li key={ind}>
+                            <span>{obj.desc}</span>
+                            <span>{obj.amount}</span>
+                        </li>)
+                    })}
+                </ul>
+            </h3>
         </div>
     )
 }
