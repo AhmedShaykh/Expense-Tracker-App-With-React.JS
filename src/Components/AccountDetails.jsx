6f4477565e2afac62aaea3f59f17ef0d35866efc
Ahmed Saleem Shaikh <AhmedShaykh@users.noreply.github.com>
2022-07-19 15:33:33 +0500

    It's Time To Push !

--- src/Components/AccountDetails.jsx
+++ src/Components/AccountDetails.jsx
@@ -2,10 +2,12 @@ function AccountDetails() {
     return (
         <div className="expense-container">
             <div>
-            <h3 className="target">INCOME <br /><span className="green">$1000</span></h3>
+            <h3 className="target">INCOME <br />
+            <span className="green">$1000</span></h3>
             </div>
             <div>
-            <h3 className="target">EXPENSE <br /><span className="red">$740</span></h3>
+            <h3 className="target">EXPENSE <br />
+            <span className="red">$740</span></h3>
             </div>
         </div>
     )
